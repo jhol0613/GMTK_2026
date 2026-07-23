867ad971d6170b6cfb0ae05661f5e160115daf3b
tec-g <nb755589@gmail.com>
2026-07-23 13:12:08 +0600

    ResshanLabel2D is now a tool script

--- godot_project/resshan_alphabet/language_display.gd
+++ godot_project/resshan_alphabet/language_display.gd
@@ -1,6 +1,11 @@
+@tool
+class_name ResshanLabel2D
 extends Node2D
 
-@export_multiline() var string:String
+@export_multiline() var string:String :
+	set(value):
+		string = value
+		queue_redraw()
 
 func _draw() -> void:
 	LanguageRenderer.draw_text(string, self)
