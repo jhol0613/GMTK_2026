757feccff5aa5087980f6754669f759379629ec9
tec-g <nb755589@gmail.com>
2026-07-24 19:06:46 +0600

    ResshanInteractable now shows has a pop up

--- godot_project/notebook/notebook_page.gd
+++ godot_project/notebook/notebook_page.gd
@@ -7,6 +7,7 @@ signal entry_removed
 const LIMIT: = 10
 
 var entries_count: int = 0
+var entries: Array[NotebookEntry] = []
 
 func _new_entry(encoded:String) -> void:
 	var entry:NotebookEntry = preload('uid://s4gdpvpyayn0').instantiate()
@@ -15,9 +16,11 @@ func _new_entry(encoded:String) -> void:
 	entry.add_resshan(encoded)
 	entry.tree_exiting.connect(_remove_entry.bind(entry))
 	entries_count += 1
+	entries.append(entry)
 	if entries_count == LIMIT:
 		limit_reached.emit()
 
-func _remove_entry(_entry:NotebookEntry) -> void:
+func _remove_entry(entry:NotebookEntry) -> void:
 	entries_count -= 1
+	entries.erase(entry)
 	entry_removed.emit()
