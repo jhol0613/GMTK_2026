cc0ba1b55d90cc1d8144937c8b9e7fc0130a7cc6
scumpi <saberfang961@gmail.com>
2026-07-24 20:56:34 +0200

    fix: missing signal

--- godot_project/autoload/signal_bus.gd
+++ godot_project/autoload/signal_bus.gd
@@ -1,6 +1,8 @@
 extends Node
 
-@warning_ignore("unused_signal")
+@warning_ignore_start("unused_signal")
 signal minutes_passed(minutes: int)
+signal train_departed
 signal resshan_clicked(encoded_resshan: String)
-signal resshan_note_requested(by_label:ResshanInteractable)
+signal resshan_note_requested(by_label: ResshanInteractable)
+@warning_ignore_restore("unused_signal")
