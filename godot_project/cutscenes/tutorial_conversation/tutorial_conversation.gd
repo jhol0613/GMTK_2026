44ccf3a5be0b82a977abcc5c872a88834bc038d8
jhol0613 <john.hollandr@gmail.com>
2026-07-24 12:48:08 -0700

    intro_convo

--- godot_project/cutscenes/tutorial_conversation/tutorial_conversation.gd
+++ godot_project/cutscenes/tutorial_conversation/tutorial_conversation.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 
+@onready var dialogue_interactable := $DialogueInteractable
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	dialogue_interactable.interact()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
