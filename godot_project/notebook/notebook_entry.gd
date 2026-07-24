961e1bc2ff0d5620abd7c51ea311e34352cf941a
tec-g <nb755589@gmail.com>
2026-07-24 17:22:11 +0600

    Big update to Notebook functionality
    Limit is 6 pages.
    Can cycle through them.
    Resshan text is added to the first free page.

--- godot_project/notebook/notebook_entry.gd
+++ godot_project/notebook/notebook_entry.gd
@@ -7,7 +7,9 @@ func _draw() -> void:
 	if not _resshan_string:
 		return
 	var shape: = LanguageRenderer.draw_resshan_text(
-		$Resshan.position + Vector2(0,24),_resshan_string,self,true)
+		$Resshan.position + Vector2(0,24), _resshan_string, self, true,
+		20, Color.BLACK
+	)
 	$Resshan.custom_minimum_size.x = shape.size.x
 
 func add_resshan(encoded:String) -> void:
