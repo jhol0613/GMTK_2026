c19469af2473c4b36cc9d132cd0ffdbb3cbcad44
scumpi <saberfang961@gmail.com>
2026-07-25 19:57:20 +0200

    feat: add time to hourglass

--- godot_project/level_elements/hourglass/hourglass.gd
+++ godot_project/level_elements/hourglass/hourglass.gd
@@ -3,6 +3,7 @@ class_name Hourglass
 
 @onready var hourglass_top: AnimatedSprite2D = $HourglassTop
 @onready var hourglass_bottom: AnimatedSprite2D = $HourglassBottom
+@onready var _time_label: ResshanLabel = $TimeLabel
 
 var _start_total: int = 0
 
@@ -13,9 +14,11 @@ func _ready() -> void:
 
 	TimeManager.time_changed.connect(_on_time_changed)
 	_sync_frames(TimeManager.hour, TimeManager.minute)
+	_update_label(TimeManager.hour, TimeManager.minute)
 
 func _on_time_changed(hour: int, minute: int) -> void:
 	_sync_frames(hour, minute)
+	_update_label(hour, minute)
 
 ## Sync the frames of the hourglass to the current time
 func _sync_frames(hour: int, minute: int) -> void:
@@ -25,6 +28,9 @@ func _sync_frames(hour: int, minute: int) -> void:
 
 	_set_frame_from_progress(hourglass_top, progress)
 	_set_frame_from_progress(hourglass_bottom, progress)
+
+func _update_label(hour: int, minute: int) -> void:
+	_time_label.text = "<<%s>> : <<%s>>" % [hour, minute]
 
 ## Set the frame of an animated sprite based on a progress value
 func _set_frame_from_progress(sprite: AnimatedSprite2D, progress: float) -> void:
