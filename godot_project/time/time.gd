29b5f48ec6ba9d9080f3afadf96a3d91079d9d8b
Juca-5 <Joline.cezanne@gmx.de>
2026-07-25 14:33:05 +0200

    time highlight

--- godot_project/time/time.gd
+++ godot_project/time/time.gd
@@ -3,11 +3,14 @@ extends Control
 const HOUR_PER_DAY: int = 8
 
 @onready var label: ResshanLabel = $Label
+@onready var highlight: Panel = $Highlight
+@onready var timer: Timer = $Timer
 
 ## Countdown starts here and ticks down towards 0:0
 @export var start_hour: int = HOUR_PER_DAY
 @export var start_minute: int = 0
 
+var tween: Tween
 
 func _ready() -> void:
 	TimeManager.time_changed.connect(_on_time_changed)
@@ -16,10 +19,26 @@ func _ready() -> void:
 
 	if TimeManager.consume_flash():
 		_flash_timer()
+	
+	highlight.visible = false
 
 
 func _on_time_changed(hour: int, minute: int) -> void:
 	_update_label(hour, minute)
+	
+	if tween and tween.is_running():
+		tween.kill()
+	
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_BACK)
+	
+	tween.tween_property(self, "scale:x", 1.2, 0.2)
+	tween.parallel().tween_property(self, "scale:y", 1.2, 0.2)
+	
+	
+	tween.tween_property(self, "scale:x", 1.0, 0.2)
+	tween.parallel().tween_property(self, "scale:y", 1.0, 0.2)
 
 
 func _update_label(hour: int, minute: int) -> void:
