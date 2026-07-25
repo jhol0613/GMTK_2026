d2fe15f611cd5f87fdfe6b0e81a5548c64c8a23f
LangyiRen <144127676+LangyiRen@users.noreply.github.com>
2026-07-25 16:37:02 -0500

    Clock Sound volume balanced

--- godot_project/time/time.gd
+++ godot_project/time/time.gd
@@ -4,16 +4,19 @@ const HOUR_PER_DAY: int = 8
 
 @export_category("Clock Sounds")
 @export var second_change_sound: AudioStream
+@export_range(-40.0, 6.0, 0.5) var second_volume_db: float = -6.0
 @export var minute_change_sound: AudioStream
+@export_range(-40.0, 6.0, 0.5) var minute_volume_db: float = -0.0
 
 @onready var label: ResshanLabel = $Label
 @onready var highlight: Panel = $Highlight
 @onready var sfx_player: AudioStreamPlayer = $SFXPlayer
 
-## Countdown starts here and ticks down towards 0:0
 @export var start_hour: int = HOUR_PER_DAY
 @export var start_minute: int = 8
 @export var start_second: int = 8
+
+
 
 var tween: Tween
 
@@ -41,13 +44,17 @@ func _on_time_changed(
 	_update_label(hour, minute, second)
 
 	if _has_previous_time:
-		# Minute 变化的优先级更高。
-		# 即使此时 Second 也从 0 变成 7，也只播放 Minute 音效。
 		if minute != _previous_minute or hour != _previous_hour:
-			_play_clock_sound(minute_change_sound)
+			_play_clock_sound(
+				minute_change_sound,
+				minute_volume_db
+			)
 
 		elif second != _previous_second:
-			_play_clock_sound(second_change_sound)
+			_play_clock_sound(
+				second_change_sound,
+				second_volume_db
+			)
 
 	_previous_hour = hour
 	_previous_minute = minute
@@ -61,11 +68,31 @@ func _on_time_changed(
 	tween.set_ease(Tween.EASE_IN)
 	tween.set_trans(Tween.TRANS_BACK)
 
-	tween.tween_property(self, "scale:x", 1.2, 0.2)
-	tween.parallel().tween_property(self, "scale:y", 1.2, 0.2)
+	tween.tween_property(
+		self,
+		"scale:x",
+		1.2,
+		0.2
+	)
+	tween.parallel().tween_property(
+		self,
+		"scale:y",
+		1.2,
+		0.2
+	)
 
-	tween.tween_property(self, "scale:x", 1.0, 0.2)
-	tween.parallel().tween_property(self, "scale:y", 1.0, 0.2)
+	tween.tween_property(
+		self,
+		"scale:x",
+		1.0,
+		0.2
+	)
+	tween.parallel().tween_property(
+		self,
+		"scale:y",
+		1.0,
+		0.2
+	)
 
 
 func _update_label(hour: int, minute: int, second: int) -> void:
@@ -79,9 +106,13 @@ func _flash_timer(flashes: int = 3, interval: float = 0.5) -> void:
 		label.modulate = Color.WHITE
 		await get_tree().create_timer(interval).timeout
 		
-func _play_clock_sound(stream: AudioStream) -> void:
+func _play_clock_sound(
+	stream: AudioStream,
+	volume_db: float
+) -> void:
 	if stream == null:
 		return
 
 	sfx_player.stream = stream
+	sfx_player.volume_db = volume_db
 	sfx_player.play()
