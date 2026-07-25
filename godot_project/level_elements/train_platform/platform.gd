124a7eecdc4f6cafe90ffc503098c1fe6b43ebef
SHIALABEOUFSPC\Gamma <304196525+gammaf88@users.noreply.github.com>
2026-07-25 17:32:13 -0500

    Level 1 polish, yellow -> green, platform QOL WIP

--- godot_project/level_elements/train_platform/platform.gd
+++ godot_project/level_elements/train_platform/platform.gd
@@ -2,17 +2,19 @@
 
 extends Node2D
 
-@export var arrival_train: Train
+@export var upper_train: Train
 @export var play_arrival_on_ready: bool = true
 @export var lower_train: Train
 
+@export var color: Enums.TrainColor
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	if Engine.is_editor_hint():
 		return
-	if arrival_train and play_arrival_on_ready:
-		arrival_train.play_arrival_animation()
+	if upper_train and play_arrival_on_ready:
+		upper_train.play_arrival_animation()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,7 +22,13 @@ func _process(_delta: float) -> void:
 	pass
 	
 func depart_upper_train():
-	arrival_train._train_depart()
+	upper_train._train_depart()
 
 func arrive_upper_train():
-	arrival_train.play_simple_arrival_animation()
+	upper_train.play_simple_arrival_animation()
+
+func depart_lower_train():
+	lower_train._train_depart()
+
+func arrive_lower_train():
+	lower_train.play_simple_arrival_animation()
