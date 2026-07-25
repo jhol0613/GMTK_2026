d9b3c73e544bdbfecb376ff40f9fc29974fc015e
LangyiRen <144127676+LangyiRen@users.noreply.github.com>
2026-07-25 11:19:39 -0500

    level 1 audio controller updated, ATM sfx updated

--- godot_project/interactables/dialogue_interactable.gd
+++ godot_project/interactables/dialogue_interactable.gd
@@ -6,8 +6,12 @@ extends Interactable
 @export var choices: Array[DialogueChoice] = []
 @export var panel_path: NodePath
 @export var minutes_cost: int = 1
+@export var turn_on_sound: AudioStreamPlayer
 
 func interact() -> void:
+	if turn_on_sound != null:
+		turn_on_sound.play()
+		
 	if panel_path.is_empty():
 		return
 	var panel := get_node_or_null(panel_path) as DialoguePanel
