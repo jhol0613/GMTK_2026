29e4162a9c40ffeeebe16c7f1968ccb641cc590e
scumpi <saberfang961@gmail.com>
2026-07-26 16:51:01 +0200

    fix: station nuimber assignment

--- godot_project/level_elements/train_platform/platform.gd
+++ godot_project/level_elements/train_platform/platform.gd
@@ -5,10 +5,17 @@ extends Node2D
 @export var upper_train: Train
 @export var play_arrival_on_ready: bool = true
 @export var lower_train: Train
+@export var station_number: int = 0:
+	set(value):
+		station_number = value
+		if is_inside_tree():
+			_apply_station_number_to_signs()
 
 # Called when the node enters the scene tree for the first time.
+func _enter_tree() -> void:
+	_apply_station_number_to_signs()
+
 func _ready() -> void:
-	
 	if Engine.is_editor_hint():
 		return
 	if upper_train and play_arrival_on_ready:
@@ -18,6 +25,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func _apply_station_number_to_signs() -> void:
+	for child in get_children():
+		if "station_number" in child:
+			child.station_number = station_number
 	
 func depart_upper_train():
 	upper_train._train_depart()
