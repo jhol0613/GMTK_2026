60780cbcdd52ba7b34b00829e9af0ba4d840efd2
scumpi <saberfang961@gmail.com>
2026-07-26 16:17:01 +0200

    feat: add text to inventory

--- godot_project/ui/inventory/inventory_panel.gd
+++ godot_project/ui/inventory/inventory_panel.gd
@@ -6,6 +6,9 @@ const SLOT_COUNT: int = 4
 @onready var _slot_icons: Array[TextureRect] = [
 	%Slot1Icon, %Slot2Icon, %Slot3Icon, %Slot4Icon,
 ]
+@onready var _slot_names: Array[ResshanLabel] = [
+	%Slot1Name, %Slot2Name, %Slot3Name, %Slot4Name,
+]
 
 
 func _ready() -> void:
@@ -21,9 +24,14 @@ func refresh() -> void:
 
 	for i in SLOT_COUNT:
 		var icon := _slot_icons[i]
+		var name_label := _slot_names[i]
 		if i < items.size():
 			icon.texture = items[i].item_icon
 			icon.visible = icon.texture != null
+			name_label.text = items[i].item_name
+			name_label.visible = not items[i].item_name.is_empty()
 		else:
 			icon.texture = null
 			icon.visible = false
+			name_label.text = ""
+			name_label.visible = false
