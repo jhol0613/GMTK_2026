798fd8a48939f2185e66cf464b1fb101730c0eca
grant <304196525+gammaf88@users.noreply.github.com>
2026-08-23 21:55:53 -0500

    Tinyfoliage animation start variance

--- godot_project/level_elements/decorative/plants/tiny_foliage.gd
+++ godot_project/level_elements/decorative/plants/tiny_foliage.gd
@@ -6,4 +6,7 @@ enum anims {shroom1, white1, yellow1}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	sprite.play(anims.keys()[anim])
+	sprite.play( anims.keys()[anim] )
+	sprite.stop()
+	await get_tree().create_timer( randf_range(0,1) ).timeout
+	sprite.play( anims.keys()[anim] )
